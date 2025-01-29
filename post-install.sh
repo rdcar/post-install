505c52797081290dc981d2448bfa32d7d2826363
Renato Carvalho <65754712+rdcar@users.noreply.github.com>
2025-01-29 02:09:15 -0300

    Update post-install.sh
    Rules for GSConnect (Zorin Connect)

--- post-install.sh
+++ post-install.sh
@@ -84,6 +84,11 @@ rm anaconda.sh
 # Consertando o problema do Dummy Output no kernel 6.8+
 echo "options snd-hda-intel dmic_detect=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf echo "blacklist snd_soc_skl" | sudo tee -a /etc/modprobe.d/blacklist.conf
 
+# Adicionando regras no Firewall para GSConnect / KDE Connect / Zorin Connect
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
+sudo ufw reload
+
 # Finalizando
 echo "Instalação concluída!"
 echo "Reinicie o sistema para garantir que todas as mudanças entrem em efeito corretamente."
